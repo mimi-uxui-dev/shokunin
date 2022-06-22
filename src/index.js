d28147098a067a5289806298942f07acaa5ee43e
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-06-22 10:42:41 +0200

    HTML -- About

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
+/**
+ * 
+ * 
+### 8. run project
+```bash
+npm start 
+
+// before deploying, run 
+
+npm run build
+git add -A
+git commit -m 'Some message'
+git push
+```
+
+ */
+
+const csjs = require('csjs-inject')
+const bel = require('bel')
+const navigation = require("./navigation.js")
+
+
+
 function start () {
-    const el = document.createElement('div')
-    el.innerText = 'Hello world'
-    document.body.appendChild(el)
-  }
-  
-  start()  
+  const el = document.createElement('div')
+  el.appendChild = 'Hello world'
+  document.body.appendChild(el)
+}
+
+start()
---
+++ src/navigation.js
@@ -0,0 +1,12 @@
+const csjs = require('csjs-inject')
+const bel = require('bel')
+
+module.exports = navigation
+
+function navigation(){
+    return bel`
+        <h1>
+            navigation
+        </h1>
+    `
+}
